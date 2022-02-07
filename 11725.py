f7ad019d81af76c89589459684fad79d687da127
jjinyeok <fun2314@naver.com>
2022-02-07 12:09:32 +0900

    first commit

---
+++ 11725.py
@@ -0,0 +1,26 @@
+from sys import stdin
+from collections import deque
+input = stdin.readline
+
+N = int(input())
+graph = [[] for _ in range(N + 1)]
+parent = [0 for _ in range(N + 1)]
+
+for i in range(N - 1):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+def bfs():
+    queue = deque()
+    queue.append(1)
+    while queue:
+        node = queue.popleft()
+        for i in graph[node]:
+            if parent[i] == 0:
+                parent[i] = node
+                queue.append(i)
+
+bfs()
+for i in parent[2:]:
+    print(i)
